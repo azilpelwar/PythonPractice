dba5774a8fc454c3fa215c3cf27711e6582dd9a2
Ankur Zilpelwar <ankur.zilpelwar@gmail.com>
2014-06-04 22:59:37 +0400

    Add string search prog

--- stringconcat.py
+++ stringconcat.py
@@ -4,7 +4,8 @@ t = 'bodacious'
 # write Python code that prints out udacious
 # without using any quote characters in
 # your code.
-print s[:3]+t[4:]
+print s[:3] + t[4:]
+print s[:-2] + t[-3:] 
 
 
 
